7fa4fb122610a73852b497a4286c1827845479fa
junuo <3236877923@qq.com>
2023-12-29 22:43:17 +0800

    【项目：网络层】【模块：core/network】 【原因：添加网络层服务器文件、客户端文件】 【改动：添加网络层服务器文件、客户端文件。初步实现一部分基本的服务器类的方法，待写线程实际共享】 【影响面：服务器基本设置，监听、新建连接等是否正常。编译能过，但是功能未验证】

---
+++ core/network/kmshareserver.cpp
@@ -0,0 +1,54 @@
+﻿#include "kmshareserver.h"
+
+QList<Device*> KMShareServer::s_deviceList;
+
+KMShareServer::KMShareServer(QObject* parent /*= nullptr*/)
+	: m_tcpServer(new QTcpServer(parent))
+{
+	connect(m_tcpServer, &QTcpServer::newConnection, this, &KMShareServer::onNewConnection);
+}
+
+KMShareServer::~KMShareServer()
+{
+	m_tcpServer->close();
+}
+
+const QList<Device*>& KMShareServer::deviceList()
+{
+	return s_deviceList;
+}
+
+quint16 KMShareServer::port() const
+{
+	return m_port;
+}
+
+void KMShareServer::setPort(qint16 port)
+{
+	m_port = port;
+}
+
+QString KMShareServer::serverAddress() const
+{
+	return m_tcpServer->serverAddress().toString();
+}
+
+bool KMShareServer::listen()
+{
+	return m_tcpServer->listen(QHostAddress::Any, m_port);
+}
+
+void KMShareServer::close()
+{
+	m_tcpServer->close();
+}
+
+void KMShareServer::onNewConnection()
+{
+	Device* device = new Device;
+	device->m_tcpSocket = m_tcpServer->nextPendingConnection();
+	device->m_address = device->m_tcpSocket->peerAddress().toString();
+	device->m_port = device->m_tcpSocket->peerPort();
+	device->m_deviceName = device->m_tcpSocket->peerName();
+	s_deviceList.append(device);
+}
---
+++ core/network/kmshareserver.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <QTcpServer>
+#include <QTcpSocket>
+
+struct Device
+{
+	bool isConnected() const 
+	{
+		return m_tcpSocket->state() == QAbstractSocket::ConnectedState;
+	}
+
+	QString m_deviceName;
+	QString m_address;
+	quint16 m_port = 0;
+	QTcpSocket* m_tcpSocket = nullptr;
+};
+
+class KMShareServer : public QObject
+{
+public:
+	KMShareServer(QObject* parent = nullptr);
+	~KMShareServer();
+	static const QList<Device*>& deviceList();
+	quint16 port() const;
+	void setPort(qint16 port);
+	QString serverAddress() const;
+	bool listen();
+	void close();
+
+private:
+	void onNewConnection();
+
+	static QList<Device*> s_deviceList;
+	quint16 m_port = 9521;
+	QTcpServer* m_tcpServer = nullptr;
+};
