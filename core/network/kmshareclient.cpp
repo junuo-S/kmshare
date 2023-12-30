458f774971fb48d48b75e7d9c612deb62f647221
junuo <3236877923@qq.com>
2023-12-30 22:20:02 +0800

    【项目：网络层-客户端】【模块：core/network】 【原因：添加客户端类】 【改动：添加客户端类，并初步实现连接、断开连接、接受消息并post的方法】 【影响面：作为接收方使用时，能否正常连接/断开连接服务端。消息能否正常模拟。编译通过，功能未验证】

--- core/network/kmshareclient.cpp
+++ core/network/kmshareclient.cpp
@@ -1,1 +1,34 @@
-﻿
+﻿#include "kmshareclient.h"
+
+#include "core/event/abstractevent.h"
+#include "core/event/mouseevent.h"
+#include "core/event/keyboardevent.h"
+
+KMShareClient::KMShareClient(QObject* parent /*= nullptr*/)
+	: QObject(parent)
+	, m_tcpSocket(new QTcpSocket(this))
+{
+	connect(m_tcpSocket, &QTcpSocket::readyRead, this, &KMShareClient::onReadyRead);
+}
+
+KMShareClient::~KMShareClient()
+{
+
+}
+
+void KMShareClient::connectToHost(const QString& hostName, quint16 port)
+{
+	m_tcpSocket->connectToHost(hostName, port);
+}
+
+void KMShareClient::disconnectFromHost()
+{
+	m_tcpSocket->disconnectFromHost();
+}
+
+void KMShareClient::onReadyRead()
+{
+	AbstractEvent* event = MouseEvent::fromJsonString(m_tcpSocket->readAll().toStdString());
+	if (event)
+		event->post();
+}
--- core/network/kmshareclient.h
+++ core/network/kmshareclient.h
@@ -1,1 +1,17 @@
-﻿
+﻿#pragma once
+
+#include <QTcpSocket>
+
+class KMShareClient : public QObject
+{
+public:
+	KMShareClient(QObject* parent = nullptr);
+	~KMShareClient();
+	void connectToHost(const QString& hostName, quint16 port);
+	void disconnectFromHost();
+
+private:
+	void onReadyRead();
+
+	QTcpSocket* m_tcpSocket = nullptr;
+};
